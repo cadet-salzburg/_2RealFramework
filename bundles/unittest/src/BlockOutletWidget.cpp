1e3afcdcf32baa36e1b2747eda41c2adb65dffc3
Robert Praxmarer <fhs31297@fh-salzburg.ac.at>
2012-07-17 13:42:36 +0000

    new datatypes supported by unittest

--- bundles/unittest/src/BlockOutletWidget.cpp
+++ bundles/unittest/src/BlockOutletWidget.cpp
@@ -9,21 +9,28 @@ BlockOutletWidget::BlockOutletWidget(_2Real::app::OutletHandle& imageHandle, QWi
 	: m_OutletHandle(imageHandle),
 	  QGroupBox(parent)
 {
-	m_Layout = new QHBoxLayout();
-	m_Layout->addWidget( new QLabel(QString::fromStdString( m_OutletHandle.getName() )) );
+	try
+	{
+		m_Layout = new QHBoxLayout();
+		m_Layout->addWidget( new QLabel(QString::fromStdString( m_OutletHandle.getName() )) );
 
-	m_ValueWidget = new QLabel(this);
-	m_Layout->addWidget( m_ValueWidget );
-	setLayout( m_Layout );
+		m_ValueWidget = new QLabel(this);
+		m_Layout->addWidget( m_ValueWidget );
+		setLayout( m_Layout );
 
-	// register data callback for _2Real Framework
-	m_OutletHandle.registerToNewData( *this, &BlockOutletWidget::receiveData );
+		// register data callback for _2Real Framework
+		m_OutletHandle.registerToNewData( *this, &BlockOutletWidget::receiveData );
 
-	// note that the moc is quite pedantic with regards to namespaces:
-	// you must use the full symbol name here & in the function implementations
-	// data between threads has to be sent around through signal and slots in QT !!!!
-	qRegisterMetaType< _2Real::app::AppData >( "_2Real::app::AppData" );
-	connect( this, SIGNAL( sendData( _2Real::app::AppData ) ), this, SLOT( updateData( _2Real::app::AppData ) ) );
+		// note that the moc is quite pedantic with regards to namespaces:
+		// you must use the full symbol name here & in the function implementations
+		// data between threads has to be sent around through signal and slots in QT !!!!
+		qRegisterMetaType< _2Real::app::AppData >( "_2Real::app::AppData" );
+		connect( this, SIGNAL( sendData( _2Real::app::AppData ) ), this, SLOT( updateData( _2Real::app::AppData ) ) );
+	}
+	catch(Exception& e)
+	{
+		cout << e.message() << e.what() << std::endl;
+	}
 }
 
 void BlockOutletWidget::receiveData(_2Real::app::AppData const& data)
@@ -37,20 +44,42 @@ void BlockOutletWidget::receiveData(_2Real::app::AppData const& data)
 
 void BlockOutletWidget::updateData(_2Real::app::AppData data) 
 {
-	if(m_OutletHandle.getTypename().find("img_uchar")==0)
-	{	
-		m_Width  = data.getData<ImageT<unsigned char>>().getWidth();
-		m_Height = data.getData<ImageT<unsigned char>>().getHeight();
-		unsigned char* ptr = data.getData<ImageT<unsigned char>>().getData();
-		m_Img = QImage( ptr, m_Width,m_Height, QImage::Format_RGB888);
-		m_Pixmap = QPixmap::fromImage(m_Img);
-
-		dynamic_cast<QLabel*>(m_ValueWidget)->setPixmap(m_Pixmap);
-	}
-	else if( m_OutletHandle.getTypename() == "int" || m_OutletHandle.getTypename() == "double" )
+	try
 	{
-		dynamic_cast<QLabel*>(m_ValueWidget)->setText(QString::fromStdString(data.getDataAsString()));
-	}
+		if(m_OutletHandle.getTypename().find("img_uchar")==0)
+		{	
+			m_Width  = data.getData<ImageT<unsigned char>>().getWidth();
+			m_Height = data.getData<ImageT<unsigned char>>().getHeight();
+			unsigned char* ptr = data.getData<ImageT<unsigned char>>().getData();
+			m_Img = QImage( ptr, m_Width,m_Height, QImage::Format_RGB888);
+			m_Pixmap = QPixmap::fromImage(m_Img);
 
-	update();
+			dynamic_cast<QLabel*>(m_ValueWidget)->setPixmap(m_Pixmap);
+		}
+		else if( m_OutletHandle.getTypename() == "short" || m_OutletHandle.getTypename() == "unsigned short" ||
+			m_OutletHandle.getTypename() == "int" || m_OutletHandle.getTypename() == "unsigned int" || 
+			m_OutletHandle.getTypename() == "long" || m_OutletHandle.getTypename() == "unsigned long" ||
+			m_OutletHandle.getTypename() == "double" || m_OutletHandle.getTypename() == "bool")
+		{
+			dynamic_cast<QLabel*>(m_ValueWidget)->setText(QString::fromStdString(data.getDataAsString()));
+		}
+		else if( m_OutletHandle.getTypename() == "char")
+		{
+			stringstream strTmp;
+			strTmp << data.getDataAsString() << " : " << data.getData<char>();
+			dynamic_cast<QLabel*>(m_ValueWidget)->setText(QString::fromStdString( strTmp.str()));
+		}
+		else if( m_OutletHandle.getTypename() == "unsigned char")
+		{
+			stringstream strTmp;
+			strTmp << data.getDataAsString() << " : " << data.getData<unsigned char>();
+			dynamic_cast<QLabel*>(m_ValueWidget)->setText(QString::fromStdString( strTmp.str()));
+		}
+
+		update();
+	}
+	catch(Exception& e)
+	{
+		cout << e.message() << e.what() << std::endl;
+	}
 }
