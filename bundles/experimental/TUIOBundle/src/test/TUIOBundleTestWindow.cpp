53c5a6f5bacd9a39bebbc69be907c49a43c93631
Otto Naderer <otto.naderer@aec.at>
2012-08-29 11:37:32 +0000

    - again update to the TUIOBundle and its test project

---
+++ bundles/experimental/TUIOBundle/src/test/TUIOBundleTestWindow.cpp
@@ -0,0 +1,107 @@
+#include <cstdio>
+#include "app/_2RealBlockHandle.h"
+#include "app/_2RealInletHandle.h"
+#include "helpers/_2RealException.h"
+#include "TUIOBundleTestWindow.h"
+
+
+#define CIRCLE_W 10.f
+
+using namespace _2Real;
+
+TUIOBundleTestWindow::TUIOBundleTestWindow() : sf::RenderWindow(sf::VideoMode(640,480), "TUIOBundleTest"), circle(CIRCLE_W), handle(0)
+{
+    winX = (float)getSize().x;
+    winY = (float)getSize().y;
+}
+
+TUIOBundleTestWindow::~TUIOBundleTestWindow()
+{
+
+}
+
+//void TUIOBundleTestWindow::setup(bundle::BlockHandle& handle)
+//{
+//    try
+//    {
+//        outletHandle = handle.getOutletHandle("TracklistOut");
+//    }
+//    catch ( Exception &e )
+//	{
+//		std::cout << e.message() << std::endl;
+//		e.rethrow();
+//	}
+//}
+
+void TUIOBundleTestWindow::update()
+{
+    tracks.clear();
+    if (sf::Mouse::isButtonPressed(sf::Mouse::Left))
+    {
+        float x = (float) sf::Mouse::getPosition(*this).x;
+        float y = (float) sf::Mouse::getPosition(*this).y;
+        if (x < 0.f || x > winX || y < 0.f || y > winY)
+        {
+            circle.setPosition(-100.f, -100.f);
+            return;
+        }
+        circle.setPosition(x - CIRCLE_W, y - CIRCLE_W);
+        printf("X: %1.2f, Y: %1.2f\n", x / winX, y / winY);
+        Point p(Number(x / winX), Number(y / winY), Number(0.f), "TRACK", 0);
+        tracks.push_back(p);
+    }
+    else
+    {
+        circle.setPosition(-100.f, -100.f);
+    }
+
+//    if (outletHandle.isValid())
+//        outletHandle.getWriteableRef<std::vector<Point>>() = tracks;
+
+//
+//    sf::Event Event;
+//    while (pollEvent(Event))
+//    {
+//        // Close window : exit
+//        if (Event.type == sf::Event::Closed)
+//            close();
+//    }
+//
+//    // Clear screen
+//    clear(sf::Color(128, 128, 196));
+//
+//    drawAll();
+//    display();
+}
+
+void TUIOBundleTestWindow::drawAll()
+{
+    clear(sf::Color(128, 128, 196));
+    draw(circle);
+    if (handle && tracks.size() > 0)
+    {
+        try
+        {
+            handle->getInletHandle("Tracklist").setValue(tracks);
+            printf("GOT INLET HANDLE!\n");
+            //handle->singleStep();
+            printf("SINGLESTEP SUCCEEDED!\n");
+        }
+        catch ( _2Real::Exception &e )
+        {
+            std::cout << e.message() << std::endl;
+            e.rethrow();
+        }
+    }
+}
+
+void TUIOBundleTestWindow::onResize()
+{
+    winX = (float)getSize().x;
+    winY = (float)getSize().y;
+}
+
+void TUIOBundleTestWindow::shutdown()
+{
+
+}
---
+++ bundles/experimental/TUIOBundle/src/test/main.cpp
@@ -0,0 +1,74 @@
+#include "TUIOBundleTestWindow.h"
+#include "_2RealApplication.h"
+#include "helpers/_2RealException.h"
+
+int main()
+{
+    _2Real::app::Engine* engine = nullptr;
+    _2Real::app::BundleHandle* tuioBundle = nullptr;
+    _2Real::app::BlockHandle* tuioBlockHandle = nullptr;
+
+    try
+    {
+        engine = &(_2Real::app::Engine::instance());
+
+        engine->setBaseDirectory("../lib");
+        tuioBundle = &(engine->loadBundle("libTUIOBundle"));
+        printf("DESC: %s\n", tuioBundle->getBundleInfo().getDescription().c_str());
+        tuioBlockHandle = &(tuioBundle->createBlockInstance("TUIOBlock"));
+        if (tuioBlockHandle->isValid())
+        {
+            printf("TUIOBlockHandle OK!\n");
+        }
+        tuioBlockHandle->setup();
+        printf("SETUP OK!\n");
+        tuioBlockHandle->setUpdateRate(30.f);
+        printf("UPDATERATE OK!\n");
+        tuioBlockHandle->start();
+        printf("START OK!\n");
+
+//        tuioBundleTest = &(engine->loadBundle("libTUIOBundleTest"));
+//        printf("DESC: %s\n", tuioBundleTest->getBundleInfo().getDescription().c_str());
+
+    }
+    catch ( _2Real::Exception &e )
+	{
+		std::cout << e.message() << std::endl;
+		e.rethrow();
+	}
+
+    TUIOBundleTestWindow app;
+    app.setVerticalSyncEnabled(true);
+
+    app.setBlockHandle(tuioBlockHandle);
+
+    while (app.isOpen())
+    {
+        // Process events
+        sf::Event Event;
+        while (app.pollEvent(Event))
+        {
+            // Close window : exit
+            if (Event.type == sf::Event::Closed)
+                app.close();
+        }
+
+        // Clear screen
+        //app.clear(sf::Color(0, 0, 255));
+
+        // Draw the sprite
+        app.update();
+
+        app.drawAll();
+        app.display();
+    }
+
+    //_2Real::app::BundleHandle* tuioBundleTest = nullptr;
+
+	engine->clearAll();
+
+
+
+
+    return 0;
+}
